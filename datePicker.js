45bcb9904e94b08c1a93fb4e9793766d33781992
miracle <dada_tl@foxmail.com>
2016-06-30 16:58:48 +0800

    Update datePicker.js

--- datePicker.js
+++ datePicker.js
@@ -28,6 +28,21 @@ function removeClass(ele, className){
 }
 
 /*****************************************************
+ * 跨浏览器兼容的获取计算后的样式的属性
+ * @param {Object} ele		元素
+ * @param {Object} after	伪元素
+ */
+function getComputedStyleAttrValue(ele,attr, after){
+	if(document.defaultView.getComputedStyle){
+		return document.defaultView.getComputedStyle(ele, after)[attr];
+	}else if(window.getComputedStyle){
+		return window.getComputedStyle(ele, after)[attr];
+	}else if(ele.currentStyle){
+		return ele.currentStyle[attr];
+	}
+}
+	
+/*****************************************************
  * @函数柯里化 为函数预先绑定参数。
  * @param {Object} fn
  * @param {Object} context
@@ -172,51 +187,135 @@ function stopPropagation(event){
 }
 
 
-
-
-//隐藏时间显示按钮
-function hideDate(id){
-	document.getElementById(id).getElementsByClassName('date-date')[0].style.display = "none";
+/***************************************************
+ * @兼容IE的显示模块操作，使用了惰性载入的方法减少了判断的次数
+ * @param {Object} id	当前元素的id
+ * @param {Object} className	操作的元素的类名
+ * @param {Object} tag			操作元素的标签名。
+ */
+function showModule(id, className, tag){
+	if(typeof document.querySelector){
+		showModule = function(id, className, tag){
+			document.querySelector("#" + id + ' .' + className).style.display = "block";
+		}
+	}else if(typeof document.getElementsByClassName){
+		showModule = function(id, className, tag){
+			document.getElementById(id).getElementsByClassName(className)[0].style.display = "block";
+		}
+	}else{
+		
+		showModule = function(id, className, tag){
+			var el = document.getElementById(id),
+				i =	0,
+				els = el.getElementsByTagName(tag);
+				len = els.length;
+				
+			for(i; i < len; i++){
+				if(els.className == className){
+					els[i].style.display = "block";
+					els = null;
+					break;
+				}
+			}
+		}
+		
+	}
+	
+	showModule(id, className, tag);
 }
 
-//显示时间显示按钮
-function showDate(id){
-	document.getElementById(id).getElementsByClassName('date-date')[0].style.display = "block";
+//隐藏模块
+function hideModule(id, className, tag){
+	
+	if(typeof document.querySelector){
+		hideModule = function(id, className, tag){
+			document.querySelector("#" + id + ' .' + className).style.display = "none";
+		}
+	}else if(typeof document.getElementsByClassName){
+		hideModule = function(id, className, tag){
+			document.getElementById(id).getElementsByClassName(className)[0].style.display = "none";
+		}
+	}else{
+		
+		hideModule = function(id, className, tag){
+			var el = document.getElementById(id),
+				i =	0,
+				els = el.getElementsByTagName(tag);
+				len = els.length;
+				
+			for(i; i < len; i++){
+				if(els.className == className){
+					els[i].style.display = "none";
+					els = null;
+					break;
+				}
+			}
+		}
+		
+	}
+	
+	hideModule(id, className, tag);
 }
 
-//隐藏天数主体
-function hideDay(id){
-	document.getElementById(id).getElementsByClassName('content-day')[0].style.display = "none";
+/***************************************************
+ * @兼容IE的获取class元素的方法
+ * @param {Object} id	当前元素的id
+ * @param {Object} className	操作的元素的类名
+ * @param {Object} tag			操作元素的标签名。
+ */
+function getClassName(id, className, tag){
+	if(typeof document.querySelector){
+		getClassName = function(id, className, tag){
+			return document.querySelector("#" + id + ' .' + className);
+		}
+	}else if(typeof document.getElementsByClassName){
+		getClassName = function(id, className, tag){
+			return document.getElementById(id).getElementsByClassName(className)[0];
+		}
+	}else{
+		
+		getClassName = function(id, className, tag){
+			var el = document.getElementById(id),
+				i =	0,
+				els = el.getElementsByTagName(tag);
+				len = els.length;
+				
+			for(i; i < len; i++){
+				if(els.className == className){
+					return els[i];
+				}
+			}
+		}
+		
+	}
+	
+	return getClassName(id, className, tag);
 }
 
-//显示天数主体
-function showDay(id){
-	document.getElementById(id).getElementsByClassName('content-day')[0].style.display = "block";
+
+//事件委托，事件处理
+function eventHandler(param, e){
+	e = getEvent(e);
+	var target = getTarget(e);
+	switch(target.className){
+		//如果是点击了年的选择框的值。
+		case "year-input":
+			var content_year = getClassName(eventHandler.param.id, 'content-year', 'div');
+			
+			if(getComputedStyleAttrValue(content_year, "display") == "none"){
+				showModule(eventHandler.param.id, 'content-year', 'div');
+			}else{
+				hideModule(eventHandler.param.id, 'content-year', 'div');
+			}
+			
+			
+			
+			content_year.scrollTop = 15 * 25 + 60 ;
+			
+		break;
+	}
+	
 }
-
-//隐藏月模块
-function hideMonth(id){
-	document.getElementById(id).getElementsByClassName('content-month')[0].style.display = "none";
-}
-
-//显示月模块
-function showMonth(id){
-	document.getElementById(id).getElementsByClassName('content-month')[0].style.display = "block";
-}
-
-//隐藏年模块
-function hideYear(id){
-	document.getElementById(id).getElementsByClassName('content-year')[0].style.display = "none";
-}
-
-//显示年模块
-function showYear(id){
-	document.getElementById(id).getElementsByClassName('content-year')[0].style.display = "block";
-}
-
-
-
-
 
 /************************************************************************************************
  * @构造函数时间选择器的基类
@@ -231,10 +330,9 @@ function DatePicker(param){
 		week : 0,
 		hours : 0,
 		minutes : 0,
-		seconds : 0
+		seconds : 0,
+		currDate : new Date()
 	};
-	
-	
 	
 	
 	
@@ -243,6 +341,7 @@ function DatePicker(param){
 		return new DatePicker(param);
 	}
 	
+	//对传入的参数的处理
 	if(typeof param === "object"){
 		
 		//定义用于存放时间选择器容器的元素。
@@ -267,12 +366,12 @@ function DatePicker(param){
 						'<div class="datePicker-icon">day</div>'+
 						'<div class="datePicker-showdate">'+
 						'<ul class="date-title">'+
-						'<li class="year-prev btn"><a href="javascript:void(0);">◄</a></li>'+
-						'<li	class="year-li text"><input type="text" name="" id="" value="2016年" /></li>'+
-						'<li class="year-next btn"><a href="javascript:void(0);">►</a></li>'+
-						'<li class="month-prev btn"><a href="javascript:void(0);">◄</a></li>'+
-						'<li	class="month-li text"><input type="text" name="" id="" value="6月" /></li>'+
-						'<li class="month-next btn"><a href="javascript:void(0);">►</a></li>'+
+						'<li class="btn"><a class="year-prev" href="javascript:void(0);">◄</a></li>'+
+						'<li class="text"><input type="text" name="" class="year-input" value="2016年" /></li>'+
+						'<li class="btn"><a class="year-next" href="javascript:void(0);">►</a></li>'+
+						'<li class="btn"><a class="month-prev" href="javascript:void(0);">◄</a></li>'+
+						'<li class="text"><input class="month-input" type="text" name="" id="" value="6月" /></li>'+
+						'<li class="btn"><a class="month-next" href="javascript:void(0);">►</a></li>'+
 						'</ul>'+
 						'<ul class="date-title2"></ul>'+
 						'<div class="data-content">'+
@@ -314,12 +413,39 @@ function DatePicker(param){
 							'<div id="close-year"><a href="javascript:void(0);">×</a></div>'+
 							'<ul>';
 			
-			for(var i = 0; i < 42; i++){
-				dateSubEle += '<li><a href="javascript:void(0);">2000年</a></li>';
+			var curr = myDate.currDate.getFullYear() - 25;
+			
+			for(var i = 0; i < 50; i++){
+				dateSubEle += '<li><a href="javascript:void(0);">' + (curr++) + '年</a></li>';
 			}
 			
-			dateSubEle += '</ul>'+
+			dateSubEle += 	'</ul>'+
 							'</div>'+
+							'<div class="content-date">' +
+							'<div class="date-check">' +
+							'<span class="cancel">取消</span>' +
+							'<span class="confirm">确定</span>' +
+							'</div>' +
+							'<div class="date-line"></div>' +
+							'<ul class="date-hours">';
+			
+			for(var i = 0; i < 24; i++){
+				dateSubEle += '<li><a href="javascript:void(0);">' + i + '</a></li>';
+			}
+			
+			dateSubEle += '</ul><ul class="date-minutes">';
+			
+			for(var i = 0; i < 60; i++){
+				dateSubEle += '<li><a href="javascript:void(0);">' + i + '</a></li>';
+			}
+			
+			dateSubEle += '</ul><ul class="date-seconds">';
+			
+			for(var i = 0; i < 60; i++){
+				dateSubEle += '<li><a href="javascript:void(0);">' + i + '</a></li>';
+			}
+			
+			dateSubEle += 
 							'</div>'+
 							'<div class="date-bottom">'+
 							'<ul class="date-date">'+
@@ -344,36 +470,68 @@ function DatePicker(param){
 		 */
 		
 		
-		//类型的处理
+		//类型的处理，显示与隐藏相关的DOM节点。
 		if(typeof param.type != "undefined"){
 			
 			switch(param.type){
 				case "Y-M-D-H" :
-					hideYear(param.id);
-					hideMonth(param.id);
-					showDate(param.id);
-					showDay(param.id);
+					//隐藏年模块
+					hideModule(param.id, 'content-year', 'div');
+					//隐藏月模块
+					hideModule(param.id, 'content-month', 'div');
+					//隐藏时间选择模块
+					hideModule(param.id, 'content-date', 'div');
+					//显示时间按钮组
+					showModule(param.id, 'date-date', 'ul');
+					//显示天数模块
+					showModule(param.id, 'content-day', 'div');
 					break;
 				case "Y-M-D" :
-					hideYear(param.id);
-					hideMonth(param.id);
-					hideDay(param.id);
-					showDate(param.id);
+					//隐藏年模块
+					hideModule(param.id, 'content-year', 'div');
+					//隐藏月模块
+					hideModule(param.id, 'content-month', 'div');
+					//隐藏时间选择模块
+					hideModule(param.id, 'content-date', 'div');
+					//显示时间按钮组
+					hideModule(param.id, 'date-date', 'ul');
+					//显示天数模块
+					showModule(param.id, 'content-day', 'div');
 					break;
 				case "Y-M" :
-					hideYear(param.id);
-					showMonth(param.id);
-					hideDay(param.id);
-					hideDate(param.id);
+				
+					//隐藏年模块
+					hideModule(param.id, 'content-year', 'div');
+					//隐藏月模块
+					showModule(param.id, 'content-month', 'div');
+					//隐藏时间选择模块
+					hideModule(param.id, 'content-date', 'div');
+					//显示时间按钮组
+					hideModule(param.id, 'date-date', 'ul');
+					//显示天数模块
+					hideModule(param.id, 'content-day', 'div');
+					
 					break;
 				case "Y" :
-					showYear(param.id);
-					hideMonth(param.id);
-					hideDay(param.id);
-					hideDate(param.id);
+					//隐藏年模块
+					showModule(param.id, 'content-year', 'div');
+					//隐藏月模块
+					hideModule(param.id, 'content-month', 'div');
+					//隐藏时间选择模块
+					hideModule(param.id, 'content-date', 'div');
+					//显示时间按钮组
+					hideModule(param.id, 'date-date', 'ul');
+					//显示天数模块
+					hideModule(param.id, 'content-day', 'div');
 					break;
 			}
 		}
+		
+		var el = document.getElementById(param.id);
+		
+		eventHandler.param = param;
+		
+		addEventHandler(el, 'click', eventHandler);
 		
 	}else{
 		throw new Error("参数类型不对!");
